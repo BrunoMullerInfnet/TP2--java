2e32b38d29432d5bad51c20df83a9f30a443091a
bruno.crespo <bruno.crespo@al.infnet.edu.br>
2025-03-10 08:10:05 -0300

    Exercício 1

---
+++ tp2/src/ex1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+
+        System.out.println("Digite o seu nome: ");
+        String nomePessoal = leitor.next();
+        int tamanhoNomePessoal = nomePessoal.length();
+
+        System.out.println("Digite a sua idade");
+        int idade = leitor.nextInt();
+        leitor.nextLine();
+
+        System.out.println("Digite o nome da sua mãe: ");
+        String nomeMae = leitor.next();
+        int tamanhoNomeMae = nomeMae.length();
+
+        System.out.println("Digite o nome do seu pai: ");
+        String nomePai = leitor.next();
+        int tamanhoNomePai = nomePai.length();
+
+        if (tamanhoNomePessoal > tamanhoNomePai || tamanhoNomePessoal > tamanhoNomeMae) {
+            System.out.println("O nome do filho é maior do que o nome dos pais");
+        } else{
+            System.out.println("O nome do filho é menor do que o nome dos pais");
+        }
+
+    }
+}
